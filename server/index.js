85de48d9db2e4d3ac1e26e7e50045019f22ec5cb
liudasC <l.cekavicius@gmail.com>
2021-03-18 15:18:07 +0200

    feat: server middleware dependencies, env, nodemon

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config({path: "server/.env"});
 
 const server = express();
+const{ SERVER_PORT } = process.env;
+
+// Middlewares
+server.use(cors());
+server.use(morgan("tiny"));
+
 
 server.get("/", (req, res) => {
     res.send("Serveris veikia")
 })
 
-server.listen(5000, () => {
-    console.log(`Cars MERN server is running on http://localhost:5000`);
+server.listen(SERVER_PORT, () => {
+    console.log(`Cars MERN server is running on http://localhost:${SERVER_PORT}`);
 })
