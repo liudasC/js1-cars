29d1035f7d767475e874be0f513c747dfa9b63b9
liudasC <l.cekavicius@gmail.com>
2021-03-22 13:06:58 +0200

    feat: crud finished

--- server/controllers/cars.js
+++ server/controllers/cars.js
@@ -1,30 +1,95 @@
-const CarModel = require("../models/CarModel")
+const mongoose = require('mongoose');
+const CarModel = require('../models/CarModel');
+
+// Galimos savybės
+const availableProps = ['brand', 'model', 'year', 'engineVolume'];
+const restrictedProps = ['_id', 'createdAt', 'updatedAt', '__v'];
+
+// Suformuoja tik galimų keisti savybių objektą. 
+const createPropsToUpdate = (props) => {
+  let propsToChange = {};
+  for (const propName in props) {
+    if (propName !== undefined && availableProps.includes(propName))
+      // if (propName !== undefined && !restrictedProps.includes(propName))
+      propsToChange[propName] = props[propName];
+  }
+  return propsToChange;
+}
 
 const getCars = async (req, res) => {
-    try {
-        const carsFromDb = awaitCarModel.find();
-        res.status(200).json({ cars: carsFromDB })
-    }
-    catch (error) {
-        res.status(404).json({ message: error.message })
-    }
+  try {
+    const carsFromDB = await CarModel.find();
+    res.status(200).json({ cars: carsFromDB });
+  }
+  catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
 
 const postCar = async (req, res) => {
-    res.status(200).json("ateityje idesiu 1 masina")
+  const { brand, model, year, engineVolume } = req.body;
+  try {
+    const newCar = await CarModel.create({ brand, model, year, engineVolume });
+    res.status(200).json({
+      car: {
+        id: newCar._id,
+        brand: newCar.brand,
+        model: newCar.model,
+        year: newCar.year,
+        engineVolume: newCar.engineVolume
+      }
+    });
+  }
+  catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
 
 const updateCar = async (req, res) => {
-    res.status(200).json("ateityje atnaujinsiu 1 masina")
+  const { id } = req.params;
+  // const propsToUpdate = createPropsToUpdate(req.body);
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new Error(`Neteisingas id '${id}' formatas`);
+    const updatedCar = await CarModel.findByIdAndUpdate(id, req.body, { new: true });
+    // const updatedCar = await CarModel.findByIdAndUpdate(id, propsToUpdate, { new: true });
+    if (updatedCar === null)
+      throw new Error(`Nepavyko rasti ir atnaujinti mašinos su id '${id}'`);
+    res.status(200).json({
+      car: updatedCar
+    });
+  }
+  catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
 
 const deleteCar = async (req, res) => {
-    res.status(200).json("ateityje istrinsiu 1 masina")
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id))
+      throw new Error(`Neteisingas id '${id}' formatas`);
+    const deletedCar = await CarModel.findByIdAndDelete(id);
+    if (deletedCar === null)
+      throw new Error(`Nepavyko rasti ir ištrinti mašinos su id '${id}'`);
+    res.status(200).json({
+      car: {
+        id: deletedCar._id,
+        brand: deletedCar.brand,
+        model: deletedCar.model,
+        year: deletedCar.year,
+        engineVolume: deletedCar.engineVolume
+      }
+    });
+  }
+  catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 }
 
 module.exports = {
-    getCars,
-    postCar,
-    updateCar,
-    deleteCar
+  getCars,
+  postCar,
+  updateCar,
+  deleteCar
 }
--- server/models/CarModel.js
+++ server/models/CarModel.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-        brand: {
-            type: String,
-            required: true
-        },
-        model: {
-            type: String,
-            required: true
-        },
-        year: {
-            type: Number,
-            required: true
-        },
-        engineVolume: {
-            type: Number,
-            required: true
-        }
+    brand: {
+        type: String,
+        required: true
     },
-    { timestamps: true });
+    model: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    engineVolume: {
+        type: Number,
+        required: true
+    }
+},
+    {
+        timestamps: true
+        // strict: false // Šis nustatymas leidžia įdėti bet kokias papildomas savybes 
+    });
 
 module.exports = mongoose.model("Car", schema)
